ce36e2dfb573f3dae71a5c5f3545f9668e54a0ce
Marcos Moura <marcosmoura201@hotmail.com>
2022-07-25 19:47:53 -0300

    Testando o projeto e inserinfo um getMapping teste

--- src/main/java/com/mmpcoder/vendasapi/VendasApiApplication.java
+++ src/main/java/com/mmpcoder/vendasapi/VendasApiApplication.java
@@ -2,12 +2,18 @@ package com.mmpcoder.vendasapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApiApplication {
 	
-	
-	
+	@GetMapping("/hello/{nome}")
+	public String hello(@PathVariable String nome) {
+		return "Primeiro teste do backend, projeto " + nome;
+	}
 	
 
 	public static void main(String[] args) {
