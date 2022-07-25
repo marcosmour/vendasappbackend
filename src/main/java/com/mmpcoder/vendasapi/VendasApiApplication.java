3f7ac3065f3c5dab0087c9f45fdd213e0284528f
Marcos Moura <marcosmoura201@hotmail.com>
2022-07-25 19:35:25 -0300

    backend do Projeto Criado

---
+++ src/main/java/com/mmpcoder/vendasapi/VendasApiApplication.java
@@ -0,0 +1,17 @@
+package com.mmpcoder.vendasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApiApplication {
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasApiApplication.class, args);
+	}
+
+}
